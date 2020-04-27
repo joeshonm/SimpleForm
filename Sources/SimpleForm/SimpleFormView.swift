be22351c773f7b854c56d35702d958b56733dd65
JoeShon Monroe <me@joeshonm.com>
2020-04-27 01:05:02 -0400

    Change SimpleForm to SimpleFormView

--- Sources/SimpleForm/SimpleFormView.swift
+++ Sources/SimpleForm/SimpleFormView.swift
@@ -1,5 +1,5 @@
 //
-//  SimpleForm.swift
+//  SimpleFormView.swift
 //  SimpleFormExample
 //
 //  Created by JoeShon Monroe on 4/26/20.
@@ -8,7 +8,7 @@
 
 import SwiftUI
 
-public struct SimpleForm: View {
+struct SimpleFormView: View {
     @ObservedObject var model:SimpleFormModel = SimpleFormModel()
     var body: some View {
         Form {
