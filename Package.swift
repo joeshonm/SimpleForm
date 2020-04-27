b50cbedae2517129dd45a4006c6e239f25a7b53d
JoeShon Monroe <me@joeshonm.com>
2020-04-27 01:21:02 -0400

    Update Package

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SimpleForm",
-            targets: ["SimpleForm","SimpleFormField"]),
+            targets: ["SimpleForm"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
