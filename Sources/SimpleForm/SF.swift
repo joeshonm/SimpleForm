78ebf3cd20a607848fdc48f90dcf2b186be89c6b
JoeShon Monroe <me@joeshonm.com>
2020-04-29 10:20:08 -0400

    Update validation result check

--- Sources/SimpleForm/SF.swift
+++ Sources/SimpleForm/SF.swift
@@ -31,9 +31,8 @@ public struct SF: View {
                     let regex = try! NSRegularExpression(pattern: #"[a-z0-9]+([-+._][a-z0-9]+){0,2}@.*?(\.(a(?:[cdefgilmnoqrstuwxz]|ero|(?:rp|si)a)|b(?:[abdefghijmnorstvwyz]iz)|c(?:[acdfghiklmnoruvxyz]|at|o(?:m|op))|d[ejkmoz]|e(?:[ceghrstu]|du)|f[ijkmor]|g(?:[abdefghilmnpqrstuwy]|ov)|h[kmnrtu]|i(?:[delmnoqrst]|n(?:fo|t))|j(?:[emop]|obs)|k[eghimnprwyz]|l[abcikrstuvy]|m(?:[acdeghklmnopqrstuvwxyz]|il|obi|useum)|n(?:[acefgilopruz]|ame|et)|o(?:m|rg)|p(?:[aefghklmnrstwy]|ro)|qa|r[eosuw]|s[abcdeghijklmnortuvyz]|t(?:[cdfghjklmnoprtvwz]|(?:rav)?el)|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw])\b){1,2}"#)
                     let validationResult = regex.firstMatch(in: (field.model.value as! String), options: [], range: range) != nil
                     
-                    print("Validation Result: ", validationResult)
                     
-                    if (validationResult) {
+                    if (validationResult == false) {
                         field.model.errors.append("Please enter a valid email address.")
                     }
                 }
